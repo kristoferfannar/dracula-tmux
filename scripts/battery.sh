e1787adc303b865cb7be24523272ecc5e35788ea
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-05-21 01:20:08 +0000

    Format battery.sh

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -2,102 +2,96 @@
 # setting the locale, some users have issues with different locales, this forces the correct one
 export LC_ALL=en_US.UTF-8
 
-current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source $current_dir/utils.sh
+current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$current_dir"/utils.sh
 
 linux_acpi() {
   arg=$1
   BAT=$(ls -d /sys/class/power_supply/*)
-  if [ ! -x "$(which acpi 2> /dev/null)" ];then
+  if [ ! -x "$(which acpi 2>/dev/null)" ]; then
     case "$arg" in
-      status)
-        cat $BAT/status
-        ;;
+    status)
+      cat "$BAT"/status
+      ;;
 
-      percent)
-        cat $BAT/capacity
-        ;;
+    percent)
+      cat "$BAT"/capacity
+      ;;
 
-      *)
-        ;;
+    *) ;;
     esac
   else
     case "$arg" in
-      status)
-        acpi | cut -d: -f2- | cut -d, -f1 | tr -d ' '
-        ;;
-      percent)
-        acpi | cut -d: -f2- | cut -d, -f2 | tr -d '% '
-        ;;
-      *)
-        ;;
+    status)
+      acpi | cut -d: -f2- | cut -d, -f1 | tr -d ' '
+      ;;
+    percent)
+      acpi | cut -d: -f2- | cut -d, -f2 | tr -d '% '
+      ;;
+    *) ;;
     esac
   fi
 }
 
-battery_percent()
-{
+battery_percent() {
   # Check OS
   case $(uname -s) in
-    Linux)
-      percent=$(linux_acpi percent)
-      [ -n "$percent" ] && echo " $percent"
-      ;;
+  Linux)
+    percent=$(linux_acpi percent)
+    [ "$percent" != "" ] && echo " $percent"
+    ;;
 
-    Darwin)
-      echo $(pmset -g batt | grep -Eo '[0-9]?[0-9]?[0-9]%')
-      ;;
+  Darwin)
+    echo "$(pmset -g batt | grep -Eo '[0-9]?[0-9]?[0-9]%')"
+    ;;
 
-    FreeBSD)
-      echo $(apm | sed '8,11d' | grep life | awk '{print $4}')
-      ;;
+  FreeBSD)
+    echo "$(apm | sed '8,11d' | grep life | awk '{print $4}')"
+    ;;
 
-    CYGWIN*|MINGW32*|MSYS*|MINGW*)
-      # leaving empty - TODO - windows compatability
-      ;;
+  CYGWIN* | MINGW32* | MSYS* | MINGW*)
+    # leaving empty - TODO - windows compatability
+    ;;
 
-    *)
-      ;;
+  *) ;;
   esac
 }
 
-battery_status()
-{
+battery_status() {
   # Check OS
   case $(uname -s) in
-    Linux)
-      status=$(linux_acpi status)
-      ;;
+  Linux)
+    status=$(linux_acpi status)
+    ;;
 
-    Darwin)
-      status=$(pmset -g batt | sed -n 2p | cut -d ';' -f 2 | tr -d " ")
-      ;;
+  Darwin)
+    status=$(pmset -g batt | sed -n 2p | cut -d ';' -f 2 | tr -d " ")
+    ;;
 
-    FreeBSD)
-      status=$(apm | sed '8,11d' | grep Status | awk '{printf $3}')
-      ;;
+  FreeBSD)
+    status=$(apm | sed '8,11d' | grep Status | awk '{printf $3}')
+    ;;
 
-    CYGWIN*|MINGW32*|MSYS*|MINGW*)
-      # leaving empty - TODO - windows compatability
-      ;;
+  CYGWIN* | MINGW32* | MSYS* | MINGW*)
+    # leaving empty - TODO - windows compatability
+    ;;
 
-    *)
-      ;;
+  *) ;;
   esac
 
   case $status in
-    discharging|Discharging)
-      echo ''
-      ;;
-    high|Full)
-      echo ''
-      ;;
-    charging|Charging)
-      echo 'AC'
-      ;;
-    *)
-      echo 'AC'
-      ;;
+  discharging | Discharging)
+    echo ''
+    ;;
+  high | Full)
+    echo ''
+    ;;
+  charging | Charging)
+    echo 'AC'
+    ;;
+  *)
+    echo 'AC'
+    ;;
   esac
   ### Old if statements didn't work on BSD, they're probably not POSIX compliant, not sure
   # if [ $status = 'discharging' ] || [ $status = 'Discharging' ]; then
@@ -109,18 +103,17 @@ battery_status()
   # fi
 }
 
-main()
-{
+main() {
   bat_stat=$(battery_status)
   bat_perc=$(battery_percent)
 
   if [ -z "$bat_stat" ]; then # Test if status is empty or not
     if [ "${bat_perc%*%}" -gt 20 ]; then
-    echo "🔋 $bat_perc"
-  else
-    echo "🪫 $bat_perc"
+      echo "🔋 $bat_perc"
+    else
+      echo "🪫 $bat_perc"
     fi
-  elif [ -z "$bat_perc" ]; then # In case it is a desktop with no battery percent, only AC power
+  elif [ "$bat_perc" = "" ]; then # In case it is a desktop with no battery percent, only AC power
     echo "⚡️ $bat_stat"
   else
     echo "⚡️ $bat_perc"
@@ -129,4 +122,3 @@ main()
 
 #run main driver program
 main
-
